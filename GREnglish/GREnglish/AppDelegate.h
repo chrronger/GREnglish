ea20a4a50ee7ad6ba831990b8b3acf427586d912
guanguanrong <1282426886@qq.com>
2016-09-13 10:40:10 +0800

    init

---
+++ GREnglish/GREnglish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GREnglish
+//
+//  Created by sen on 16/9/13.
+//  Copyright © 2016年 sen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GREnglish/GREnglish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GREnglish
+//
+//  Created by sen on 16/9/13.
+//  Copyright © 2016年 sen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
